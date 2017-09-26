2e52eeebd24a4e62f34089ccf78c499e959ebfc7
panxo316 <francisco.cordero.c@gmail.com>
2017-09-26 17:38:03 -0300

    Funcion fecha en texto
    Convierte una fecha predeterminada en texto

---
+++ fecha_texto.php
@@ -0,0 +1,26 @@
+<?php
+
+$fecha = '07-03-1987';
+echo obtenerFechaEnLetra($fecha);
+
+
+
+
+function obtenerFechaEnLetra($fecha){
+    $dia= conocerDiaSemanaFecha($fecha);
+    $num = date("j", strtotime($fecha));
+    $anno = date("Y", strtotime($fecha));
+    $mes = array('enero', 'febrero', 'marzo', 'abril', 'mayo', 'junio', 'julio', 'agosto', 'septiembre', 'octubre', 'noviembre', 'diciembre');
+    $mes = $mes[(date('m', strtotime($fecha))*1)-1];
+    return $num.' de '.ucwords($mes).' de '.$anno;
+}
+ 
+function conocerDiaSemanaFecha($fecha) {
+    $dias = array('Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado');
+    $dia = $dias[date('w', strtotime($fecha))];
+    return $dia;
+}
+
+
+
+?>
